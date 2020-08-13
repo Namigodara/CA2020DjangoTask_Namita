27dcdfe8c56935365bd199f03c6aecb3c7f106ab
Namigodara <namigodara@gmail.com>
2020-08-13 12:32:06 -0700

    django assignment_1

--- Learning_Project/Learning_App/models.py
+++ Learning_Project/Learning_App/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Info(models.Model):
     Name = models.CharField("Full Name", max_length = 50)
-    email = models.EmailField("Email address", max_length = 75)
+    ID = models.AutoField(primary_key = True, null = False)
+    # email = models.EmailField("Email address", max_length = 75)
     Contact = models.IntegerField("Contact Number")
     Address = models.CharField( max_length = 400)
 
